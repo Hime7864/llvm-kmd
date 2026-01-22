2bc120210a541010467ab0a01ce0a93404d28f69
Hime7864 <159477578+Hime7864@users.noreply.github.com>
2026-01-22 12:45:34 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -2,11 +2,6 @@
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
-	printf("DriverEntry called.\n");
-	FWA::Initialize();
 
-	printf("FW Allocator initialized. Free pages: %u\n", FWA::pages_free());
-
-	FWA::Cleanup();
 	return STATUS_SUCCESS;
 }
