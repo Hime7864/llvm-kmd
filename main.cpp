fa121b83f92bec1737b960442a975ef4ddf6194f
Hime7864 <159477578+Hime7864@users.noreply.github.com>
2026-02-23 13:22:29 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -2,35 +2,6 @@
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
-	SEGMENT_REGISTER gdtr;
-	__sgdt(&gdtr);
-	printf("GDTR Base: 0x%llx, Limit: 0x%04x\n", gdtr.Base, gdtr.Limit);
-	SEGMENT_REGISTER idtr;
-	__sidt(&idtr);
-	printf("IDTR Base: 0x%llx, Limit: 0x%04x\n", idtr.Base, idtr.Limit);
-
-	auto gdtEntry = (SEGMENT_DESCRIPTOR*)gdtr.Base;
-	auto idtEntry = (IDT_GATE*)idtr.Base;
-
-	auto cs = __readcs();
-	printf("CS: %x\n", cs >> 3);
-
-	auto tr = __str();
-	printf("TR: %x\n", tr >> 3);
-
-
-	printf("index 2 access  %x\n", gdtEntry[2].access);
-	printf("index 2 flags %x\n", gdtEntry[2].flags);
-
-
-	auto ist = (INTERUPT_STACK_TABLE*)gdtEntry[4].base();
-	printf("ist->IOPB: %x\n", ist->IOPB);
-
-	printf("idtEntry[2].dpl %x\n", idtEntry[2].dpl);
-	printf("idtEntry[2].ist %x\n", idtEntry[2].ist);
-	printf("idtEntry[2].p %x\n", idtEntry[2].p);
-	printf("idtEntry[2].selector %x\n", idtEntry[2].selector);
-	printf("idtEntry[2].type %x\n", idtEntry[2].type);
-
+	printf("%i\n", MSR::PSTATE(0).get_frequency_mhz());
 	return STATUS_SUCCESS;
 }
