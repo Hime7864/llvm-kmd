b4205f18b5c5d60848822d7fb4300b902b92da7e
Hime7864 <159477578+Hime7864@users.noreply.github.com>
2026-02-23 19:34:42 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -165,7 +165,7 @@ void __attribute__((preserve_most)) SVM::VmExit(VCORE* vCore)
 	_mm_mfence();
 
 	//auto tsc = __rdtsc();
-	auto tsc_delta = (long long)((double)cpuMHz / 3);
+	auto tsc_delta = 1575;
 
 	if (exitCode == VMEXIT_INTR)
 	{
