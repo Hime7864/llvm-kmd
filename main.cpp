595ff82c04585e52a8eadbe16a34688a1bd5e2e0
Hime7864 <159477578+Hime7864@users.noreply.github.com>
2026-02-24 15:13:42 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,66 @@
 #include <intrinsics.hpp>
 
+struct CORE_TSC
+{
+	UINT64 tsc;
+	UINT64 init_latency;
+	UINT64 avg_latency;
+	UINT64 pstate;
+	UINT64 MHz;
+	double mutipler;
+};
+
+void ipi(CORE_TSC* tsc)
+{
+	auto aperf_init = MSR::APERF();
+	auto mperf_init = MSR::MPERF();
+	auto id = CPUID::current_core_number();
+	tsc[id].tsc = __rdtsc();
+	auto count = __rdtsc();
+	auto efer = MSR::EFER();
+	count = __rdtsc() - count;
+	tsc[id].init_latency = count;
+
+	UINT64 avg = 0;
+	for (int i = 0; i < 10000; i++)
+	{
+		count = __rdtsc();
+		MSR::EFER();
+		count = __rdtsc() - count;
+		avg += count;
+	}
+	avg /= 10000;
+	auto perf = MSR::APERF() - aperf_init;
+	auto mperf = MSR::MPERF() - mperf_init;
+
+	tsc[id].mutipler = (double)perf / (double)mperf;
+	tsc[id].avg_latency = avg;
+	tsc[id].pstate = MSR::PSTATE_STATUS().CurPstate;
+	tsc[id].MHz = MSR::PSTATE(tsc[id].pstate).get_frequency_mhz();
+	return;
+}
+
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
+	auto tsc = (CORE_TSC*)ExAllocatePool(NonPagedPool, 0x1000);
+	for (int i = 0; i < 100; i++)
+	{
+		KeIpiGenericCall(ipi, tsc);
+		INT64 _min, _max;
+		_min = -1ll;
+		_max = 0ll;
+		for (int i = 0; i < KeQueryActiveProcessorCount(0); i++)
+		{
+			auto MHz = (long long)((double)tsc[i].MHz * tsc[i].mutipler);
+			printf("(%i, %.i)", MHz / 100, tsc[i].avg_latency);
+			//printf("%lli[%02d | %i MHz], Avg: %i, Init: %i \n", tsc[i].tsc, i + 1, MHz, tsc[i].avg_latency, tsc[i].init_latency);
+			if (tsc[i].tsc < _min)
+				_min = tsc[i].tsc;
+			if (tsc[i].tsc > _max)
+				_max = tsc[i].tsc;
+		}
+		Sleep(10);
+	}
+	ExFreePool(tsc);
 	return STATUS_SUCCESS;
 }
