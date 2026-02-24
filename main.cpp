2292e29eba35a18fe927ebb445ec22aa9486ed6c
Steven Richter <159477578+Hime7864@users.noreply.github.com>
2026-02-23 17:40:01 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
-	printf("%i\n", MSR::PSTATE(0).get_frequency_mhz());
+	auto pstate = MSR::PSTATE(0);
+
+	printf("CpuDfsId %i\n", pstate.CpuDfsId);
+	printf("CpuFid %i\n", pstate.CpuFid);
+	printf("CpuVid %i\n", pstate.CpuVid);
+	printf("IddDiv %i\n", pstate.IddDiv);
+	printf("IddValue %i\n", pstate.IddValue);
+	printf("PstateEn %i\n", pstate.PstateEn);
 	return STATUS_SUCCESS;
 }
