c6e8d1fea6fb5408caa78547c0f9deecaf0d4a0e
Hime7864 <159477578+Hime7864@users.noreply.github.com>
2026-02-23 17:44:35 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -2,13 +2,5 @@
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
-	auto pstate = MSR::PSTATE(0);
-
-	printf("CpuDfsId %i\n", pstate.CpuDfsId);
-	printf("CpuFid %i\n", pstate.CpuFid);
-	printf("CpuVid %i\n", pstate.CpuVid);
-	printf("IddDiv %i\n", pstate.IddDiv);
-	printf("IddValue %i\n", pstate.IddValue);
-	printf("PstateEn %i\n", pstate.PstateEn);
 	return STATUS_SUCCESS;
 }
