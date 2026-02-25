f7128308f29c2021144291b52e1da857273e9ccf
Hime7864 <159477578+Hime7864@users.noreply.github.com>
2026-02-24 20:39:56 -0700

    Commit

--- main.cpp
+++ main.cpp
@@ -1,82 +1,140 @@
 #include <intrinsics.hpp>
 
-struct CORE_TSC
+void efer_read_ipi(UINT64* min)
 {
-	UINT64 tsc;
-	UINT64 init_latency;
-	UINT64 avg_latency;
-	UINT64 min_latency;
-	UINT64 pstate;
-	UINT64 MHz;
-	double mutipler;
-};
-
-void efer_ipi(CORE_TSC* tsc)
-{
-	auto aperf_init = MSR::APERF();
-	auto mperf_init = MSR::MPERF();
 	auto id = CPUID::current_core_number();
-	tsc[id].tsc = __rdtsc();
-	auto count = __rdtsc();
-	auto efer = MSR::EFER();
-	count = __rdtsc() - count;
-	tsc[id].init_latency = count;
-	tsc[id].min_latency = 0xFFFFFFFF;
-	UINT64 avg = 0;
+	min[id] = 0xFFFFFFFF;
 	for (int i = 0; i < 10000; i++)
 	{
-		count = __rdtsc();
+		auto count = __rdtsc();
 		MSR::EFER();
 		count = __rdtsc() - count;
-		if(count < tsc[id].min_latency)
-			tsc[id].min_latency = count;
-		avg += count;
+		if(min[id] > count)
+			min[id] = count;
 	}
-	avg /= 10000;
-	auto perf = MSR::APERF() - aperf_init;
-	auto mperf = MSR::MPERF() - mperf_init;
-
-	tsc[id].mutipler = (double)perf / (double)mperf;
-	tsc[id].avg_latency = avg;
-	tsc[id].pstate = MSR::PSTATE_STATUS().CurPstate;
-	tsc[id].MHz = MSR::PSTATE(tsc[id].pstate).get_frequency_mhz();
 	return;
 }
 
-void measure_efer()
+UINT64 efer_read()
 {
-	auto efer_tsc = ExAllocatePool(NonPagedPool, 0x1000);
+	auto efer_tsc = (UINT64*)ExAllocatePool(NonPagedPool, 0x1000);
 
-	KeIpiGenericCall(efer_ipi, efer_tsc);
+	KeIpiGenericCall(efer_read_ipi, efer_tsc);
 
-	UINT64 delta = 0;
-	int count = 0;
+	UINT64 min = 0xFFFFFFFF;
 	for (int i = 0; i < KeQueryActiveProcessorCount(0); i++)
 	{
-		if (((CORE_TSC*)efer_tsc)[i].pstate == 0 && ((CORE_TSC*)efer_tsc)[i].mutipler > 1.0)
-		{
-			printf("Core %d: %llu %llu %llu\n",
-				i,
-				((CORE_TSC*)efer_tsc)[i].init_latency,
-				((CORE_TSC*)efer_tsc)[i].avg_latency,
-				((CORE_TSC*)efer_tsc)[i].min_latency,
-				((CORE_TSC*)efer_tsc)[i].pstate,
-				((CORE_TSC*)efer_tsc)[i].MHz,
-				((CORE_TSC*)efer_tsc)[i].mutipler
-			);
-			delta += ((CORE_TSC*)efer_tsc)[i].min_latency;
-			count++;
-		}
+		if(min > efer_tsc[i])
+			min = efer_tsc[i];
 	}
-	delta /= count;
-	printf("Average EFER latency: %llu\n", delta);
 
 	ExFreePool(efer_tsc);
+	return min - 50;
+}
+
+void efer_write_ipi(UINT64* min)
+{
+	auto id = CPUID::current_core_number();
+	const auto efer = MSR::EFER();
+	min[id] = 0xFFFFFFFF;
+	for (int i = 0; i < 10000; i++)
+	{
+		auto count = __rdtsc();
+		MSR::EFER(efer);
+		count = __rdtsc() - count;
+		if (min[id] > count)
+			min[id] = count;
+	}
+	return;
+}
+
+UINT64 efer_write()
+{
+	auto efer_tsc = (UINT64*)ExAllocatePool(NonPagedPool, 0x1000);
+
+	KeIpiGenericCall(efer_write_ipi, efer_tsc);
+
+	UINT64 min = 0xFFFFFFFF;
+	for (int i = 0; i < KeQueryActiveProcessorCount(0); i++)
+	{
+		if (min > efer_tsc[i])
+			min = efer_tsc[i];
+	}
+
+	ExFreePool(efer_tsc);
+	return min - 50;
+}
+
+void hsave_read_ipi(UINT64* min)
+{
+	auto id = CPUID::current_core_number();
+	min[id] = 0xFFFFFFFF;
+	for (int i = 0; i < 10000; i++)
+	{
+		auto count = __rdtsc();
+		MSR::HSAVE_PA();
+		count = __rdtsc() - count;
+		if (min[id] > count)
+			min[id] = count;
+	}
+	return;
+}
+
+UINT64 hsave_read()
+{
+	auto hsave_tsc = (UINT64*)ExAllocatePool(NonPagedPool, 0x1000);
+
+	KeIpiGenericCall(hsave_read_ipi, hsave_tsc);
+
+	UINT64 min = 0xFFFFFFFF;
+	for (int i = 0; i < KeQueryActiveProcessorCount(0); i++)
+	{
+		if (min > hsave_tsc[i])
+			min = hsave_tsc[i];
+	}
+
+	ExFreePool(hsave_tsc);
+	return min - 50;
+}
+
+void hsave_write_ipi(UINT64* min)
+{
+	auto id = CPUID::current_core_number();
+	const auto hsave = MSR::HSAVE_PA();
+	min[id] = 0xFFFFFFFF;
+	for (int i = 0; i < 10000; i++)
+	{
+		auto count = __rdtsc();
+		MSR::HSAVE_PA(hsave);
+		count = __rdtsc() - count;
+		if (min[id] > count)
+			min[id] = count;
+	}
+	return;
+}
+
+UINT64 hsave_write()
+{
+	auto hsave_tsc = (UINT64*)ExAllocatePool(NonPagedPool, 0x1000);
+
+	KeIpiGenericCall(hsave_write_ipi, hsave_tsc);
+
+	UINT64 min = 0xFFFFFFFF;
+	for (int i = 0; i < KeQueryActiveProcessorCount(0); i++)
+	{
+		if (min > hsave_tsc[i])
+			min = hsave_tsc[i];
+	}
+
+	ExFreePool(hsave_tsc);
+	return min - 50;
 }
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
-	measure_efer();
-
+	printf("EFER read  : %lli\n", efer_read());
+	printf("EFER write : %lli\n", efer_write());
+	printf("HSAVE read : %lli\n", hsave_read());
+	printf("HSAVE write: %lli\n", hsave_write());
 	return STATUS_SUCCESS;
 }
