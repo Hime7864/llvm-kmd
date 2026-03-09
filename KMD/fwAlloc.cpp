2a6d47139f2bd318c38824305b9679dfea256697
Steven Richter <159477578+Hime7864@users.noreply.github.com>
2026-03-09 16:13:24 -0700

    Commit

--- KMD/fwAlloc.cpp
+++ KMD/fwAlloc.cpp
@@ -4,33 +4,6 @@ PHYSICAL_MEMORY_RANGE FWA::fw_range[31];
 UINT64 FWA::fw_range_count = 0;
 UINT64 FWA::page_idx = 0;
 
-bool NAKED FWA::is_zero_page(PVOID page)
-{
-	__asm
-	{
-		vpxor ymm0, ymm0, ymm0
-		mov eax, 4096 / 32            
-	loop:
-		vmovdqu ymm1, [rcx]           
-		vpxor ymm0, ymm0, ymm1   
-		add rcx, 32
-		vptest ymm0, ymm0         
-		jnz nonzero               
-	
-		dec eax
-		jnz loop
-	
-		mov al, 1
-		vzeroupper                
-		ret
-	
-	nonzero:
-		xor eax, eax                  
-		vzeroupper                
-		ret
-	}
-}
-
 void FWA::Initialize()
 {
 	if (page_idx || fw_range_count)
@@ -54,38 +27,42 @@ void FWA::Initialize()
 		auto rva = (UINT64)MmMapIoSpace(low, size, MmNonCached);
 		if (rva)
 		{
-			UINT64 start_rva = 0x0;
-			bool should_exit = false;
+			UINT64 first_image_end = 0;
+			UINT64 second_image_base = 0;
 			for (UINT64 current = rva; current < rva + size; current += 4096)
 			{
 				__invlpg((PVOID)current);
-				if (is_zero_page((PVOID)current))
-				{
-					if (!start_rva)
-						start_rva = current;
-				}
-				else
-				{
-					if (fw_range_count < 31 && start_rva && (current - start_rva) > 0x200000)
-					{
-						fw_range[fw_range_count].BaseAddress.QuadPart = (start_rva - rva) + low;
-						fw_range[fw_range_count].NumberOfBytes.QuadPart = current - start_rva;
-						fw_range_count++;
-					}
-					start_rva = 0;
-					UINT64 hdr_offset = *(BYTE*)(current + 0x3C);
-					if (*(UINT16*)current == 0x5A4D && // 'MZ' Hdr
-						*(UINT16*)(current + hdr_offset + 4) == 0x8664)// AMD64
-					{
-						auto sizeofimage = *(UINT32*)(current + hdr_offset + 0x50);
-						current += (sizeofimage & ~0xFFF) + (sizeofimage & 0xFFF ? 0x1000 : 0) - 4096;
 
-						should_exit = true;
+				UINT64 hdr_offset = *(BYTE*)(current + 0x3C);
+				if (*(UINT16*)current == 0x5A4D && // 'MZ' Hdr
+					*(UINT16*)(current + hdr_offset + 4) == 0x8664)// AMD64
+				{
+					auto sizeofimage = *(UINT32*)(current + hdr_offset + 0x50);
+					auto aligned_size = (sizeofimage + 0xFFF) & ~0xFFF;
+					auto pa_base = (current - rva) + low;
+					auto pa_end = pa_base + aligned_size;
+
+					if (!first_image_end)
+					{
+						first_image_end = pa_end;
 					}
+					else
+					{
+						second_image_base = pa_base;
+						if (second_image_base > first_image_end)
+						{
+							fw_range[0].BaseAddress.QuadPart = first_image_end;
+							fw_range[0].NumberOfBytes.QuadPart = second_image_base - first_image_end;
+							fw_range_count = 1;
+						}
+						break;
+					}
+
+					current += aligned_size - 4096;
 				}
 			}
 			MmUnmapIoSpace((PVOID)rva, size);
-			if (should_exit)
+			if (fw_range_count)
 				return;
 		}
 		pmr++;
--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath)
 {
+	FWA::Initialize();
+	auto alloc = FWA::ReservePages(1);
+	auto pages = FWA::pages_free();
+	printf("[+] Allocated page at %llX, %u pages free\n", alloc, pages);
+	FWA::Cleanup(1);
 	printf("[+] Driver loaded %X\n", __readcr3());
 	return STATUS_SUCCESS;
 }
